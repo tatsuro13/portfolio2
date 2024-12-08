fa30bfe45829e26ad220b8b181f0f8a0086652d2
Tatsuro Osuka <oscarwild13@gmail.com>
2024-12-08 19:49:40 +0900

    ユーティリティ関数の追加、スタイルの改善、コンポーネントの追加と設定を行いました

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import {Hind} from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const hind = Hind({
+  subsets: ["latin"],
+  variable: "--font-hind",
+  weight: ['400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
@@ -26,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${hind.variable} antialiased`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,42 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+	container:{
+		center:true,
+		padding:'15px',
+		screens:{
+			sm:'640px',
+			md:'768px',
+			lg:'1024px',
+			xl:'1280px'
+		}
+	},
+	fontFamily: {
+		primary: 'var(--font-hind)',
+	},
+  	extend: {
+  		colors: {
+  			primary: '#1c1c22',
+  			secondary: '#f8f8f8',
+  			accent: {
+				DEFAULT:'#00ff99',
+				hover:'#00e187'
+			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
