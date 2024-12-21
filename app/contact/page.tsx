cccbf56c65676d7b30341663e663197c0ef8fee9
Tatsuro Osuka <oscarwild13@gmail.com>
2024-12-21 20:04:44 +0900

    連絡先ページと作業ページコンポーネントを追加

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react"
+
+const Contact: FC = () => {
+  return (
+    <div>contact page</div>
+  )
+}
+
+export default Contact
---
+++ app/work/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react"
+
+const Work: FC = () => {
+  return (
+    <div>work page</div>
+  )
+}
+
+export default Work
