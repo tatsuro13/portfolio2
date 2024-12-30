238a2c150e5c5c6eb11138ec27c74633d421bca0
Tatsuro Osuka <oscarwild13@gmail.com>
2024-12-30 16:20:16 +0900

    Resumeコンポーネントを更新し、経験、スキル、自己紹介のタブを追加

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,165 @@
+"use client";
+
 import type { FC } from "react";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+import {
+  SiTypescript,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiAdobeillustrator,
+  SiAdobephotoshop,
+  SiAwslambda,
+  SiMysql,
+  SiRuby,
+  SiRubyonrails,
+} from "react-icons/si";
+import { motion } from "framer-motion";
+//components
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+//about me
+const about = {
+  title: "About Me",
+  description:
+    "I am a web application developer with a focus on front-end development. I strive to incorporate the latest technologies in my development. I have experience with HTML, CSS, JavaScript, TypeScript, React, Next.js, Tailwind CSS, Figma, and Node.js.",
+  info: [
+    { fieldName: "Name", value: "Sixth Project" },
+    { fieldName: "Experience", value: "13+ Years" },
+    { fieldName: "Nationality", value: "Japan" },
+    { fieldName: "Email", value: "*****@example.com" },
+    { fieldName: "Language", value: "Japanese, English" },
+  ],
+};
+
+//experience
+const experience = {
+  icon: "",
+  title: "My experience",
+  description:
+    "I have experience with HTML, CSS, JavaScript, TypeScript, React, Next.js, Tailwind CSS, Figma, and Node.js.",
+  items: [
+    {
+      company: "Company A",
+      position: "Full Stack Developer (mainly Front-end Developer)",
+      period: "2022 - Present",
+    },
+    {
+      company: "Company B",
+      position: "Front-end Developer",
+      period: "2019 - 2021",
+    },
+    {
+      company: "Company C",
+      position: "Web Designer",
+      period: "2016 - 2018",
+    },
+    {
+      company: "Company D",
+      position: "Web Designer",
+      period: "2013 - 2015",
+    },
+  ],
+};
+
+//skills
+const skills = {
+  title: "My skills",
+  description:
+    "I have experience with HTML, CSS, JavaScript, TypeScript, React, Next.js, Tailwind CSS, Figma, and Node.js.",
+  items: [
+    { name: "JavaScript", icon: <FaJs /> },
+    { name: "TypeScript", icon: <SiTypescript /> },
+    { name: "HTML 3", icon: <FaHtml5 /> },
+    { name: "CSS 3", icon: <FaCss3 /> },
+    { name: "React", icon: <FaReact /> },
+    { name: "Node.js", icon: <FaNodeJs /> },
+    { name: "AWS Lambda", icon: <SiAwslambda /> },
+    { name: "MySQL", icon: <SiMysql /> },
+    { name: "Next.js", icon: <SiNextdotjs /> },
+    { name: "Tailwind CSS", icon: <SiTailwindcss /> },
+    { name: "Ruby", icon: <SiRuby /> },
+    { name: "Ruby on Rails", icon: <SiRubyonrails /> },
+    { name: "Figma", icon: <FaFigma /> },
+    { name: "Adobe Illustrator", icon: <SiAdobeillustrator /> },
+    { name: "Adobe Photoshop", icon: <SiAdobephotoshop /> },
+  ],
+};
 
 const Resume: FC = () => {
-  return <div>resume page</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { duration: 0.5, delay: 0.5, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-16"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About Me</TabsTrigger>
+          </TabsList>
+
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-8 text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+                    {experience.items.map((item, index) => (
+                      <li
+                        key={index}
+                        className="flex flex-col gap-1 text-white/80 h-[184px] py-6 px-10 rounded-xl bg-[#27272c] justify-center
+                        items-center lg:items-start"
+                      >
+                        <p className="text-accent">{item.period}</p>
+                        <h3 className="text-xl font-bold max-w-[260px] min-h-[60px]">
+                          {item.position}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.company}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              skills
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              about
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
