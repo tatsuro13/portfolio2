94c9d1bf2af8a8eb8e05c11a7ae9f796201860c1
Tatsuro Osuka <oscarwild13@gmail.com>
2025-01-02 18:12:24 +0900

    TOPにタイプアニメーションを追加し、関連パッケージをインストール

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import type { FC } from "react";
+import { TypeAnimation } from "react-type-animation";
 import { MdOutlineEmail } from "react-icons/md";
 
 //components
+import { Button } from "@/components/ui/button";
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
 import Stats from "@/components/Stats";
@@ -15,10 +18,27 @@ const Home: FC = () => {
         <div className="flex flex-col xl:flex-row justify-between items-center xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Web Application Developer</span>
-            <h1 className="h1">
+            <h1 className="h1 text-accent">
               {`Hello I'm`}
               <br />
-              <span className="text-accent">Sixth Project</span>
+              <TypeAnimation
+                className="text-white"
+                sequence={[
+                  "Sixth Project",
+                  1000,
+                  "Web Developer",
+                  1000,
+                  "Frontend Developer",
+                  1000,
+                  "UI/UX Designer",
+                  1000,
+                  "Web Designer",
+                  1000,
+                ]}
+                wrapper="span"
+                repeat={Infinity}
+                speed={30}
+              />
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I mainly focus on front-end development with an emphasis on UI/UX.
